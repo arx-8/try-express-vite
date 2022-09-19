85d56b9954f32522558edbc5995c9ef398249bbb
arx-8 <swans.randomness@gmail.com>
2022-09-19 09:05:52 +0900

    feat: run on docker

--- README.md
+++ README.md
@@ -11,3 +11,34 @@ yarn dev
 > `⛔ Missing one or more of the following dependencies: tar, gpg`  
 > Run the following cmd:  
 > `$ brew install gpg`
+
+## Run on Docker
+
+### Run all
+
+```sh
+docker-compose -f ./deployments/docker-compose.yml up -d --build
+
+# down
+docker-compose -f ./deployments/docker-compose.yml down
+
+# (clean up garbages sometimes)
+docker image prune
+```
+
+### Tips
+
+#### Explorer inside the container
+
+modify the Dockerfile
+
+```diff
+- FROM gcr.io/distroless/nodejs:18
++ FROM gcr.io/distroless/nodejs:18-debug
+```
+
+and exec the following cmd
+
+```sh
+docker exec -it deployments-app-1 /busybox/sh
+```
--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,27 @@ import { indexGet } from "src/handlers"
 
 const app = express()
 
+// define handlers
 app.get("/", indexGet)
 
+// run for production
 if (import.meta.env.PROD) {
-  // eslint-disable-next-line @typescript-eslint/no-magic-numbers
-  app.listen(3000)
+  const port = 30001
+  const server = app.listen(port, () => {
+    console.log(`⚡️ Server is running on port ${port}!`)
+  })
+
+  // For Docker graceful shutdown
+  const shutdownGraceful: NodeJS.SignalsListener = (signal) => {
+    console.log(`⚡️ Received signal to terminate: ${signal}`)
+    server.close(() => {
+      console.log("⚡️ Server terminated gracefully.")
+      process.exit()
+    })
+  }
+  process.on("SIGINT", shutdownGraceful)
+  process.on("SIGTERM", shutdownGraceful)
 }
 
+// run for development
 export const viteNodeApp = app
